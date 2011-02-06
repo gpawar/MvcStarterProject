0f08ccdec106230d22b4e851b4c657086013df53
Jon Kruger <github@jonkruger.com>
2011-02-06 14:59:25 -0500

    created project, registered StructureMap

---
+++ src/MvcStarterProject/Config/StructureMap/StructureMapBootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using StructureMap;
+
+namespace MvcStarterProject.Config.StructureMap
+{
+    public class StructureMapBootstrapper
+    {
+        public void Bootstrap()
+        {
+            IContainer container = new Container(
+                x =>
+                    {
+
+                    });
+            DependencyResolver.SetResolver(new StructureMapDependencyResolver(container));
+        }
+    }
+}
